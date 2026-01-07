37117fbb93be1f310bdcc7849249494f7b0bf837
VanshKirtishahi <vanshkirtishahi@gmail.com>
2026-01-08 00:14:12 +0530

    Fix: Install missing deps and disable strict type checking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { AppWrapper } from "./components/common/PageMeta.tsx";
+import App from "./App";
+import { AppWrapper } from "./components/common/PageMeta";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
